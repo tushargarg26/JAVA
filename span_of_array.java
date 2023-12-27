b47a0b9d131f899a896db3ea4207440b5eee3a6e
Tushar Garg <tushargarg36977@gmail.com>
2023-12-27 23:20:30 +0530

    Question-Finding Span of Array

---
+++ span_of_array.java
@@ -0,0 +1,37 @@
+// Span- Difference between Largest and Smallest elemnet of the array
+
+import java.util.Scanner;
+public class span_of_array {
+     public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter size of the array-");
+        int n= sc.nextInt();
+        int[] arr=new int[n];
+    
+        // intiliazing the array
+        System.out.println("Enter Elements in the array-");
+        for(int i=0; i<arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+        //printing the array
+           System.out.println("Elements in the array are-");
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+"     ");
+        }
+        //finding max and min element of the array
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]>max)
+            max=arr[i];
+
+            if(arr[i]<min)
+            min=arr[i];
+        }
+        System.out.println();
+        int span= max-min;
+         System.out.println("Span of Array= "+span);
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args){
+    int x=56;
+    System.out.println(x);
+}
+}
