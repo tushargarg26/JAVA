077b2399f7dafe295e02aa14cedca317006d5db0
Tushar Garg <tushargarg36977@gmail.com>
2023-12-27 23:21:52 +0530

    Question-Finding Span of Array

--- span_of_array.java
+++ span_of_array.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 public class span_of_array {
-     public static void main(String[] args) throws Exception {
+     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         
         System.out.println("Enter size of the array-");
